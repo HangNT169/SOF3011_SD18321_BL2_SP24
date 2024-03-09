6732d3809e9497daee10579a5ef7de5c3b5f7f13
HangNT169 <HangNT169@fpt.edu.vn>
2024-03-09 17:56:50 +0700

    Buổi 3: Làm việc với form

--- src/main/java/com/sof3011/hangnt/B2_LamViecVoiForm/LoginServlet.java
+++ src/main/java/com/sof3011/hangnt/B2_LamViecVoiForm/LoginServlet.java
@@ -1,4 +1,4 @@
-package com.sof3011.hangnt;
+package com.sof3011.hangnt.B2_LamViecVoiForm;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(value = "/login")
+@WebServlet(value = {"/login","/ket-qua"}) // Value co the chua nhieu duong dan
 // Value => Duong dan.
 // Duong dan bat buoc co dau / => k co source => k build dc tomcat
 // BE & FE trao doi thong qua phuong thuc HTTP
@@ -24,6 +24,9 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Truyen gia tri tu servlet => jsp => setAttribute
+        String mess ="Hoc lai roi";
+        request.setAttribute("a",mess);
         // Chuyen trang de hien thi du lieu
         // C1:
         request.getRequestDispatcher("form-login.jsp").forward(request,response);
@@ -31,6 +34,13 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // B1: Lay gia tri tu phia jsp ve servlet => getParameter => cac o input phai co name
+        String name = request.getParameter("username");
+        String pass = request.getParameter("password");
+        // B2: Day gia tri tu phia servlet => jsp
+        request.setAttribute("x1",name);
+        request.setAttribute("x2",pass);
+        // B3: Chuyen trang
+        request.getRequestDispatcher("/ket-qua.jsp").forward(request,response);
     }
 }
---
+++ src/main/webapp/ket-qua.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hangnt
+  Date: 09/03/2024
+  Time: 17:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${x1}
+${x2}
+</body>
+</html>
